237969a389cce3cca55b4c72d5c8ff6b0cdec0e0
Arjun <arjun.saxena.as2002@gmail.com>
2022-05-29 11:12:28 +0530

    day[1] of Flutter

---
+++ lib/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class homePage extends StatelessWidget {
+  const homePage({Key? key}) : super(key: key);
+  final String name = "Arjun Saxena";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Basic Application"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
